92334aad24c042dda2ba4f6c482caf8b70defa0d
Kyxxn <gywns626@naver.com>
2024-03-24 03:32:04 +0900

    [Chore] gitignore Config & API KEY plist

---
+++ Nostalgia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nostalgia
+//
+//  Created by 박효준 on 3/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
